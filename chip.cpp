1fd8fb6d3b62abb2aa07c2fb384c572cb7ef1213
anuraagvaranasi <anuraag.varanasi@gmail.com>
2017-12-15 21:44:51 +0530

    worked out the basics for opcodes and implemented the first 3 (wooo only 34 more!)

--- chip.cpp
+++ chip.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv){
 	chip8.initialise(argv[1]); //probably add some safety incase called without arg
 	chip8.debugInfo();
 	chip8.emulate();
-	chip8.debugMem();
+	chip8.debugInfo();
 	return 0;
 }
 
@@ -16,19 +16,19 @@ int main(int argc, char **argv){
 //print debugging information
 //(except for memory cause too big)
 void Chip8::debugInfo(){
-	std::cout << "opcode = " << opcode << "\n";
+	std::cout << "opcode = " << std::hex << opcode << "\n";
 	std::cout << "V Registers: ";
 	for(int x = 0;x < 16;++x){
-		std::cout << (int)V[x] << " "; 
+		std::cout << std::hex << (int)V[x] << " "; 
 	}
 	std::cout << "\n";
 	std::cout << "I = " << I << "\n";
-	std::cout << "PC = " << PC << "\n";
+	std::cout << "PC = " << std::hex << PC << "\n";
 	std::cout << "delay timer = " << (int)delay_timer << "\n";
 	std::cout << "sound timer = " << (int)sound_timer << "\n";
 	std::cout << "stack: ";
 	for(int x = 0;x < 16;++x){
-		std::cout << stack[x] << " ";
+		std::cout << std::hex << stack[x] << " ";
 	}
 	std::cout << "\n";
 	std::cout << "SP = " << SP << "\n";
@@ -56,7 +56,7 @@ void Chip8::initialise(char* game){
 	PC = 0x200;//program counter stars at mem 0x200
 	for(int y = 0;y < 32;++y){
 		for(int x = 0;x < 64;++x){
-			gfx[x][y] = 0;
+			screen[x][y] = 0;
 		}
 	}
 	delay_timer = 0;
@@ -93,7 +93,7 @@ void Chip8::printScreen(){
 	for(int x = 0;x < 50;++x) std::cout << "\n";
 	for(int y = 0;y < 32;++y){
 		for(int x = 0;x < 64;++x){
-			if(gfx[x][y]) std::cout << "*";
+			if(screen[x][y]) std::cout << "*";
 			else std::cout << " ";
 		}
 		std::cout << "\n";
@@ -108,8 +108,36 @@ void Chip8::emulate(){
 
 	//Fetch:
 	//break into 2 parts, since its a 2 byte code
-	//first byte is all we need to determine opcode function too
-	opcode1 = memory[PC];
-	opcode2 = memory[PC+1];
+	//first byte is usually to determine opcode function 
+	memory[PC] = 0x22;
+	memory[PC+1] = 0x22;
+	opcode = memory[PC] << 8 | memory[PC+1];
+	char opcode_byte = opcode & 0xF000;
 	//Decode and Execute done in a switch statement
+	switch(opcode_byte){
+		case 0x0://3 different opcodes for 0
+			//00E0 = clear screen
+			if((opcode & 0xFF) == 0xE0){
+				for(int y = 0;y < 32;++y){
+					for(int x = 0;x < 64;++x){
+						screen[x][y] = 0;
+					}
+				}
+			}
+			//00EE = return
+			else if((opcode & 0x00FF) == 0xEE){
+				--SP;
+				PC = stack[SP];
+			}
+			//call 0nnn, where nnn is address
+			else{
+				stack[SP] = PC;
+				++SP;
+				PC = memory[opcode&0x0FFF];
+			}
+			break;
+		default: 
+			std::cerr << "An error has occurred. An opcode that does not exist has been called\n";
+			//exit(1);
+	}
 }
--- chip.h
+++ chip.h
@@ -15,7 +15,7 @@ private:
 	unsigned char V[16];//V0-14 is registers, V15 is carry flag
 	unsigned short I;//index register
 	unsigned short PC;//program counter
-	unsigned char gfx[64][32];//pixels of chip8, black/white is 0/1
+	unsigned char screen[64][32];//pixels of chip8, black/white is 0/1
 	unsigned char delay_timer;
 	unsigned char sound_timer;//both timers count down at 60hz
 	unsigned short stack[16];//only 16 bytes, anything more overflows
