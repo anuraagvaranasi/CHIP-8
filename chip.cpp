87921cc48e6ba6db464cdbf95351389fa1419617
anuraagvaranasi <anuraag.varanasi@gmail.com>
2017-12-15 20:55:01 +0530

    added initialise function and some debugging information, need to decode opcodes now

--- chip.cpp
+++ chip.cpp
@@ -1,16 +1,115 @@
 #include "chip.h"
 #include <iostream>
-#include <regex>
+#include <fstream>
 
+int main(int argc, char **argv){
 
-int main(void){
-
-	std::string s("0FED");
-	std::regex exp("^0([0-9A-F]{3})$");
-	std::smatch sm;
-	std::regex_match(s,sm,exp);
-	std::cout << sm[0] << "\n";
-
-	
+	Chip8 chip8;
+	chip8.initialise(argv[1]); //probably add some safety incase called without arg
+	chip8.debugInfo();
+	chip8.emulate();
+	chip8.debugMem();
 	return 0;
+}
+
+
+//print debugging information
+//(except for memory cause too big)
+void Chip8::debugInfo(){
+	std::cout << "opcode = " << opcode << "\n";
+	std::cout << "V Registers: ";
+	for(int x = 0;x < 16;++x){
+		std::cout << (int)V[x] << " "; 
+	}
+	std::cout << "\n";
+	std::cout << "I = " << I << "\n";
+	std::cout << "PC = " << PC << "\n";
+	std::cout << "delay timer = " << (int)delay_timer << "\n";
+	std::cout << "sound timer = " << (int)sound_timer << "\n";
+	std::cout << "stack: ";
+	for(int x = 0;x < 16;++x){
+		std::cout << stack[x] << " ";
+	}
+	std::cout << "\n";
+	std::cout << "SP = " << SP << "\n";
+
+}
+
+//only the memory since it covers whole screen
+void Chip8::debugMem(){
+	for(int x = 0; x < 4096; ++x){
+		std::cout << std::hex << (int)memory[x];
+	}
+	std::cout << "\n";
+}
+//initial system setup
+void Chip8::initialise(char* game){
+	//first clear all memory
+	opcode = 0;
+	for(int x = 0;x < 4096;++x){
+		memory[x] = 0;
+	}
+	for(int x = 0;x < 16;++x){
+		V[x] = 0;
+	}
+	I = 0;
+	PC = 0x200;//program counter stars at mem 0x200
+	for(int y = 0;y < 32;++y){
+		for(int x = 0;x < 64;++x){
+			gfx[x][y] = 0;
+		}
+	}
+	delay_timer = 0;
+	sound_timer = 0;
+	for(int x = 0;x < 16;++x){
+		stack[x] = 0;
+	}
+	SP = 0;
+	for(int x = 0;x < 16;++x){
+		keypad[x] = 0;
+	}
+
+	//load chip8 fontset into memory
+	for(int x = 0;x < 80;++x){
+		memory[x] = chip8_fontset[x];
+	}
+
+	//concatenate string with .rom
+	std::string file = std::string(game) + ".c8";
+	//now load game
+	std::ifstream fs;
+	fs.open(file);
+	char c;
+	int index = 0;
+	while(fs >> c){
+		memory[index+512] = c;
+		index++;
+	}
+	fs.close();
+}
+
+//print display
+void Chip8::printScreen(){
+	for(int x = 0;x < 50;++x) std::cout << "\n";
+	for(int y = 0;y < 32;++y){
+		for(int x = 0;x < 64;++x){
+			if(gfx[x][y]) std::cout << "*";
+			else std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
+//emulate a cycle
+void Chip8::emulate(){
+	//Fetch
+	//Decode
+	//Execute
+
+	//Fetch:
+	//break into 2 parts, since its a 2 byte code
+	//first byte is all we need to determine opcode function too
+	opcode1 = memory[PC];
+	opcode2 = memory[PC+1];
+	//Decode and Execute done in a switch statement
 }
--- chip.h
+++ chip.h
@@ -4,19 +4,43 @@
 
 class Chip8{
 public:
-
+	void initialise(char* game);
+	void printScreen();
+	void debugInfo();
+	void debugMem();
+	void emulate();
 private:
-	unsigned short opcode;//stores current opcode
+	unsigned short opcode;//current opcode
 	unsigned char memory[4096];//4K memory emulated
 	unsigned char V[16];//V0-14 is registers, V15 is carry flag
 	unsigned short I;//index register
 	unsigned short PC;//program counter
-	unsigned char gfx[64*32];//pixels of chip8, black/white is 0/1
-	unsigned char delay_timer;//timer for delay
+	unsigned char gfx[64][32];//pixels of chip8, black/white is 0/1
+	unsigned char delay_timer;
 	unsigned char sound_timer;//both timers count down at 60hz
-	unsigned short stack[16];//stack of 16 bytes
-	unsigned short sp;//stack pointer
-	unsigned char key[16];//keypad
+	unsigned short stack[16];//only 16 bytes, anything more overflows
+	unsigned short SP;//stack pointer
+	unsigned char keypad[16];
+
+	unsigned char chip8_fontset[80] =
+{ 
+    0xF0, 0x90, 0x90, 0x90, 0xF0, //0
+    0x20, 0x60, 0x20, 0x20, 0x70, //1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, //3
+    0x90, 0x90, 0xF0, 0x10, 0x10, //4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, //5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, //6
+    0xF0, 0x10, 0x20, 0x40, 0x40, //7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, //8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, //9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, //A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, //B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, //C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, //D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, //E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  //F
+};
 
 };
 
